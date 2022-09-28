cc9adda02cdc9d9e07cf0fa302de7f83e10922bb
hietpasd <davidhietpas@gmail.com>
2022-09-28 09:30:30 -0500

    added ldap props for admins to view

--- polklibrary.standards/src/polklibrary/standards/browser/user_check.py
+++ polklibrary.standards/src/polklibrary/standards/browser/user_check.py
@@ -10,13 +10,27 @@ class UserCheckView(BrowserView):
 
    
     def __call__(self):
-        output = "."
+        output = ""
+        output += "Checking Input: " + self.request.get('netid','abcdefg').lower()
         users = api.user.get_users()
-        user = api.user.get(userid=self.request.get('netid','abcdefg')) #, username=None)
+        user = api.user.get(username=self.request.get('netid','abcdefg').lower().replace('@uwosh.edu',''))
 
-            
-            
+        try:
+            output += "\n    User NetID: " + str(user.getProperty('login'))
+        except:
+            pass
+        try:
+            output += "\n    User Name: " + str(user.getProperty('displayname'))
+        except:
+            pass
+        try:
+            output += "\n    User Card Number: " + str(user.getProperty('uwoshkoshlibraryid'))
+        except:
+            output += "\n    User Card Number: missing"
+
+
         return output
+
         
         
     @property
